fe1dbdf45aa5d6f49902c9cc0fd59be601e4edff
Baribor <justluvia@gmail.com>
2023-03-11 21:32:29 +0100

    fix: form

--- src/pages/payment/payment.jsx
+++ src/pages/payment/payment.jsx
@@ -34,7 +34,7 @@ function Payment() {
                 className="shadow appearance-none border rounded w-full py-2 px-3
                 text-gray-700 leading-tight focus:outline-none  
                 focus:shadow-outline" id="firstName" type="text" 
-                placeholder="Email" name='firstName' required
+                placeholder="First Name" name='firstName' required
                 />
             </div>
             <div className='mb-4 flex flex-col text-start'>
@@ -46,7 +46,7 @@ function Payment() {
                 className="shadow appearance-none border rounded w-full py-2 px-3
                 text-gray-700 leading-tight focus:outline-none  
                 focus:shadow-outline" id="lastName" type="text" 
-                placeholder="Email" name='lastName' required
+                placeholder="Last Name" name='lastName' required
                 />
             </div>
             <div className='mb-4 flex flex-col text-start'>
